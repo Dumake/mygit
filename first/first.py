aeed484a61fd50ddc2223c4b54a51570549650bd
luoguifeng <luoguifeng14@126.com>
2019-07-18 10:44:52 +0800

    test1 file

---
+++ first/first.py
@@ -0,0 +1,12 @@
+# 本程序所使用的语言为Python,版本号为3.7
+# _*_encoding: utf-8 _*_
+# @Author    : Luo Guifeng
+# @Email     : luoguifeng14@126.com
+# @Project   : mygit
+# @File      : first
+# @Date      : 2019/7/17
+# @Time      : 10:38
+# @IDE       : PyCharm2018.3
+
+print("hello world! first")
+print('second test')
